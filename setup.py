61f4f8b2214fa6bdb7a3e6a60e76f68274d72870
Sripranav <sripranav@1cloudhub.com>
2021-01-12 14:06:35 +0530

    Initial Load

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ requires = [
 
 setup(
     name='py_aws_helper',
-    version='1.4',
+    version='1.5',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Helper for AWS services like S3 written in python...',
     long_description=open('README.md').read(),
-    install_requires=['boto3'],
+    install_requires=requires,
     url='https://github.com/1CloudHub/py_aws_helper.git',
     author='Sripranav P',
     author_email='sripranav@1cloudhub.com'
