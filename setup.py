1287615ec654bf040d2becbb597b1fde1a7a7035
Sripranav <sripranav@1cloudhub.com>
2021-01-12 14:24:38 +0530

    Initial Load

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 requires = [
-    'boto3>=1.14.49,<1.15.0'
-    'botocore<1.18.0,>=1.17.49',
-    'jmespath>=0.7.1,<1.0.0',
-    's3transfer<0.2.0,>=0.1.0'
+    'boto3>=1.16.52,<1.17.0'
+    'botocore>=1.19.52,<1.20.0',
+    'jmespath>=0.10.0,<0.11.0',
+    's3transfer>=0.3.4,<0.4.0'
 ]
 
 
 setup(
     name='py_aws_helper',
-    version='1.6',
+    version='1.7',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Helper for AWS services like S3 written in python...',
