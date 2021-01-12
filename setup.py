c2627cb43e0fa72b77bf50dcd0e4f91c581220ec
Sripranav <sripranav@1cloudhub.com>
2021-01-12 12:11:38 +0530

    Initial Load

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='py_aws_helper',
-    version='1.1',
+    version='1.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Helper for AWS services like S3 written in python...',
